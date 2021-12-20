997a756d07573e4aa7d2b56fbd73011087c9234e
Alvaro <alvinweeb@gmail.com>
2021-12-20 18:59:53 -0300

    first commit

---
+++ filter/main.js
@@ -0,0 +1,22 @@
+// filter
+
+// dobrar valores
+const numbers = [2, 4, 5, 12, 45]
+
+function dobrarNumeros(number) {
+  return number * 2
+}
+
+const dobrar = numbers.map(dobrarNumeros)
+
+// console.log(dobrar);
+
+
+// conversor de fahrenheint para celsius 
+const fahrenheint = [0, 32, 45, 50, 75, 80, 120]
+
+const convertToCelsius = fahrenheint.map( number => Math.round((number - 32) * 5/9))
+
+
+console.log(convertToCelsius);
+
