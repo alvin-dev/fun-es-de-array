5e172dfb74010c8482d6b9f68ae0e6869888d5cc
Alvaro <alvinweeb@gmail.com>
2021-12-24 20:47:50 -0300

    filter

--- filter/main.js
+++ filter/main.js
@@ -3,4 +3,6 @@
 // remover duplicados 
 const numbers = [2, 3, 4, 5, 4, 12, 19, 7, 2, 5]
 
-const removeDup = numbers.filter( (elem, index, arr) => arr.indexOf )
+const removeDup = numbers.filter( (elem, index, arr) => arr.indexOf(elem) === index )
+
+console.log(removeDup);
